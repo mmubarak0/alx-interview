2ed7f6bab51659a7e0bafae8961df5153c910baf
Mohammed Mubarak <modymu9@gmail.com>
2024-08-09 01:41:06 +0200

    star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+const getPeople = async (id) => {
+  const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+  await request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log(JSON.parse(body).name);
+      });
+    });
+  });
+};
+
+function main () {
+  const id = process.argv[2];
+  getPeople(id);
+}
+
+main();
